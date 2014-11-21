c665f9d4084286db0fa2ea8a42c0471642de9827
Vitaliy Syrchikov <maddoger@gmail.com>
2014-11-21 14:48:52 +0400

    Теги

--- frontend/views/page/index.php
+++ frontend/views/page/index.php
@@ -4,8 +4,8 @@
 /* @var $model \maddoger\website\common\models\Page */
 
 $this->title = $model->window_title ?: $model->title;
-$this->registerMetaTag(['name' => 'keywords', 'value' => $model->meta_keywords]);
-$this->registerMetaTag(['name' => 'description', 'value' => $model->meta_description]);
+$this->registerMetaTag(['name' => 'keywords', 'value' => $model->meta_keywords], 'keywords');
+$this->registerMetaTag(['name' => 'description', 'value' => $model->meta_description], 'description');
 
 echo '<div class="website-'.preg_replace('/[^\w-]/', '', $model->slug).'">';
 echo '<h2>' . $model->title . '</h2>';
